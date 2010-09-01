ea975219dd8e53f163a3280c26dd62acbfc08758
Alexandr Kuzmin <shurik@alex-desktop.(none)>
2010-08-31 18:28:39 -0600

    Grid python script is written.

--- Grid/grid.py
+++ Grid/grid.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import os
-from subprocess import call
+import subprocess
 
 if __name__=="__main__":
     print os.getcwd()
@@ -11,8 +11,26 @@ if __name__=="__main__":
         os.chdir(dir_temp)
         os.mkdir("Pressure")
         os.mkdir("Force")
-        subprocess.Popen('cp ../binary_simple.cpp Force/', shell=True)
-        subprocess.Popen('cp ../binary_guo_pressure_bb_wall.cpp Pressure/', shell=True)
-
+        subprocess.Popen('cp ../../binary_simple.cpp Force/', shell=True)
+        subprocess.Popen('cp ../../binary_guo_pressure_bb_walls.cpp Pressure/', shell=True)
+        os.chdir("Force")
+        os.mkdir("tmp2")
+        cmd="sed -e 's/const int NY=[0-9]*;/const int NY="+str(49*i+2)+";/' -e 's/const int NX=[0-9]*;/const int NX="+str(15*49*i+1)+";/'"
+        #print cmd
+        sed=subprocess.Popen(cmd+" binary_simple.cpp > binary_simple_out.cpp", shell=True)
+        sed.wait()
+        print os.getcwd()
+        comp=subprocess.Popen("g++ -O3 binary_simple_out.cpp -o main.out")
+        comp.wait()
+        subprocess.Popen("./main.out "+str(6*i)+" "+str(i)+" &")
+        os.chdir("..")
+        os.chdir("Pressure")
+        os.mkdir("tmp")
+        sed=subprocess.Popen(cmd+" binary_guo_pressure_bb_walls.cpp > binary_guo_pressure_bb_walls_out.cpp",  shell=True)
+        sed.wait()
+        comp=subprocess.Popen("g++ -O3 binary_guo_pressure_bb_walls_out.cpp -o main.out")
+        comp.wait()
+        subprocess.Popen("./main.out "+str(6*i)+" "+str(i)+" &")
+        os.chdir("..")
         os.chdir("..")
         
