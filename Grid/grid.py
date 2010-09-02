2bcf7a51f2ae1065ec76c9dc5e71358163d4de96
Alexandr Kuzmin <shurik@alex-desktop.(none)>
2010-09-02 17:48:29 -0600

    Added grid profiler.

--- Grid/grid.py
+++ Grid/grid.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 import os
 import subprocess
-
-if __name__=="__main__":
+import pylab
+import numpy
+ 
+def Run_Simulations():
     print os.getcwd()
     #os.mkdir("temp")
     for i in range(1, 5):
@@ -33,4 +35,25 @@ if __name__=="__main__":
         subprocess.Popen("./main.out "+str(6*i)+" "+str(i)+" &")
         os.chdir("..")
         os.chdir("..")
-        
+
+def Analyze_Simulations():
+    print os.getcwd()
+    from scipy.interpolate import UnivariateSpline
+    
+    for i in range(1, 4):
+        dir_temp=str(49*i+2)
+        os.chdir(dir_temp)
+        os.chdir("Force")
+        pylab.figure()
+        name="phase"+(1-(i*40000)/100000)*"0"+str(40000*i)+".dat"
+        array=numpy.loadtxt(name)
+        pylab.imshow(array)
+        pylab.figure()
+        pylab.plot(array[:, 600*i])
+        #extrapolator=UnivariateSpline(array[0:(49*i+2)/2, 600*i], numpy.arange(0, (49*i+2)/2),  k=2)
+        #print extrapolator(0)
+
+        os.chdir("../..")
+if __name__=="__main__":
+    Analyze_Simulations()    
+    pylab.show()
