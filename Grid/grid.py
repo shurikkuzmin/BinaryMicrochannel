f50d0adc679e19f066b9f6628510d8870fdfde9f
Alexandr Kuzmin <shurik@alex-desktop.(none)>
2010-09-03 11:29:09 -0600

    Grid refinement analysis is performed through the Python script.

--- Grid/grid.py
+++ Grid/grid.py
@@ -36,11 +36,19 @@ def Run_Simulations():
         os.chdir("..")
         os.chdir("..")
 
+def Get_Zero(prof, factor):
+    zero=0
+    #pylab.figure()
+    #pylab.plot(prof)
+    for counter in range(0, len(prof)/2):
+        if prof[counter]>=0 and prof[counter+1]<0:
+            zero=-(prof[counter]*(counter+1)-prof[counter+1]*counter)/(prof[counter+1]-prof[counter])
+    print (zero-0.5)/(49*factor)
+
 def Analyze_Simulations():
     print os.getcwd()
-    from scipy.interpolate import UnivariateSpline
     
-    for i in range(1, 4):
+    for i in range(1, 5):
         dir_temp=str(49*i+2)
         os.chdir(dir_temp)
         os.chdir("Force")
@@ -50,10 +58,32 @@ def Analyze_Simulations():
         pylab.imshow(array)
         pylab.figure()
         pylab.plot(array[:, 600*i])
+        pylab.savefig("grid_phase_prof_"+str(49*i)+".eps", dpi=300)
+        Get_Zero(array[:, 600*i], i)
         #extrapolator=UnivariateSpline(array[0:(49*i+2)/2, 600*i], numpy.arange(0, (49*i+2)/2),  k=2)
         #print extrapolator(0)
 
         os.chdir("../..")
+
+def Analyze_Velocities():
+    print os.getcwd()
+    
+    for i in range(1, 5):
+        dir_temp=str(49*i+2)
+        os.chdir(dir_temp)
+        os.chdir("Force")
+        name="velocity"+(1-(i*40000)/100000)*"0"+str(40000*i)+".dat"
+        array=numpy.loadtxt(name)
+        prof=array[:,100]
+        pylab.figure()
+        pylab.imshow(array)
+        pylab.figure()
+        pylab.plot(array[:, 100])
+        print prof[len(prof)/2]
+        os.chdir("../..")
+
+
 if __name__=="__main__":
-    Analyze_Simulations()    
+    #Analyze_Simulations()    
+    Analyze_Velocities()
     pylab.show()
